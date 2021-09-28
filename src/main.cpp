c9c734fefb79abb29e47d28e7e69bd3846f7c53e
rileyrg <rileyrg@gmx.de>
2021-09-28 09:03:41 +0200

    NOT working with arduino framework and stm32cube (see ARDUINO_ARCH_STM32 build_flags in platformio.ini)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#ifdef ARDUINO_ARCH_STM32
+
 #include <Arduino.h>
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,5 +20,39 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);
 
    // Wait for a second
-  delay(50);
+  delay(150);
 }
+
+#else
+
+#include "main.h"
+
+void LED_Init();
+
+int main(void) {
+  HAL_Init();
+  LED_Init();
+  while (1)
+  {
+    HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+    HAL_Delay(100);
+    static long c =0;
+    c++;
+  }
+}
+
+void LED_Init() {
+  LED_GPIO_CLK_ENABLE();
+  GPIO_InitTypeDef GPIO_InitStruct;
+  GPIO_InitStruct.Pin = LED_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+}
+
+void SysTick_Handler(void) {
+  HAL_IncTick();
+}
+
+#endif
