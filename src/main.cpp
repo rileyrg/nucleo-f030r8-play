f3fb56cfc38d9f0a4a4a0f45e40d173b2debae08
rileyrg <rileyrg@gmx.de>
2021-09-29 22:30:03 +0200

    post debian kernel update the stm32cube build doesnt work anymore

--- src/main.cpp
+++ src/main.cpp
@@ -2,46 +2,66 @@
 
 #include <Arduino.h>
 
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-   // Initialize LED pin as an output.
+  // Initialize LED pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-    // Set the LED HIGH
+void loop()
+{
+  // Set the LED HIGH
   digitalWrite(LED_BUILTIN, HIGH);
 
   // Wait for a second
-  delay(200);
+  delay(50);
 
   // Set the LED LOW
   digitalWrite(LED_BUILTIN, LOW);
 
-   // Wait for a second
-  delay(150);
+  // Wait for a second
+  delay(50);
 }
 
 #else
 
 #include "main.h"
 
+
 void LED_Init();
 
-int main(void) {
+void HAL_Delay2(uint32_t milliseconds) {
+
+   /* Initially clear flag */
+
+   (void) SysTick->CTRL;
+
+   while (milliseconds != 0) {
+
+      /* COUNTFLAG returns 1 if timer counted to 0 since the last flag read */
+
+      milliseconds -= (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) >> SysTick_CTRL_COUNTFLAG_Pos;
+
+   }
+
+}
+
+int main(void)
+{
+
   HAL_Init();
   LED_Init();
+
   while (1)
   {
-    HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
-    HAL_Delay(100);
-    static long c =0;
-    c++;
+       HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+       HAL_Delay(1000);
   }
 }
 
-void LED_Init() {
+void LED_Init()
+{
   LED_GPIO_CLK_ENABLE();
   GPIO_InitTypeDef GPIO_InitStruct;
   GPIO_InitStruct.Pin = LED_PIN;
@@ -51,7 +71,8 @@ void LED_Init() {
   HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
 }
 
-void SysTick_Handler(void) {
+void SysTick_Handler(void)
+{
   HAL_IncTick();
 }
 
